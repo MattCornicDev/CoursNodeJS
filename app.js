05f4d79fe63ffe47f545141fadaa8e9d889c5b4b
MattCornicDev <glgharib@gmail.com>
2022-10-23 16:10:12 +0200

    middleware simple et fonction next

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+
+app.use((req, res, next) => {
+    console.log('la requete à bien été reçu !')
+    next();
+})
+
+app.use((req, res, next) => {
     res.json({ message: 'Votre requête a bien été reçue !' }); 
+    next()
  });
+
+ app.use((req, res) => {
+    console.log('reponse recu')
+ })
 
 module.exports = app;
